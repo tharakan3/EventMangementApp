d24b208fec76ac7ed9cf8edee98ad066efaf6d82
kaushik <kaushikks333@gmail.com>
2018-11-02 17:38:37 -0400

    Adding Time picker and File addition UI

--- app/src/main/java/com/example/kevin/eventapp/EventActivity.java
+++ app/src/main/java/com/example/kevin/eventapp/EventActivity.java
@@ -1,27 +1,39 @@
 package com.example.kevin.eventapp;
 
+import android.Manifest;
 import android.app.DatePickerDialog;
 import android.app.Dialog;
+import android.app.TimePickerDialog;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.DialogFragment;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.format.DateFormat;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.TimePicker;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.nbsp.materialfilepicker.MaterialFilePicker;
+import com.nbsp.materialfilepicker.ui.FilePickerActivity;
 
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
+import java.util.regex.Pattern;
 
 public class EventActivity extends AppCompatActivity {
 
@@ -36,12 +48,16 @@ public class EventActivity extends AppCompatActivity {
     private Calendar calendar;
     private TextView startDate;
     private int year, month, day;
+    private static int hour, minute;
 
-    private Button mAddEventButton;
+    private Button mAddEventButton, msetTimeButton, mAddImageButton;
 
     private String userid;
 
     private Event event;
+
+    public static final int PERMISSIONS_REQUEST_CODE = 0;
+    public static final int FILE_PICKER_REQUEST_CODE = 1;
 
     FirebaseFirestore db = FirebaseFirestore.getInstance();
 
@@ -61,6 +77,24 @@ public class EventActivity extends AppCompatActivity {
         calendar = Calendar.getInstance();
         year = calendar.get(Calendar.YEAR);
 
+        mAddImageButton = (Button) findViewById(R.id.add_image);
+
+        mAddImageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        msetTimeButton = (Button) findViewById((R.id.add_time));
+
+        msetTimeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showTimePickerDialog(view);
+            }
+        });
+
         mAddEventButton = (Button) findViewById(R.id.add_event);
 
         mAddEventButton.setOnClickListener(new View.OnClickListener() {
@@ -69,6 +103,8 @@ public class EventActivity extends AppCompatActivity {
                 event = new Event();
                 event.setName(mEventName.getText().toString());
                 event.setTags(mTags.getText().toString());
+
+                //event.setDate();
                 List<String> userids = new ArrayList<String>();
                 userids.add(userid);
                 event.setOrganiserId(userid);
@@ -136,6 +172,94 @@ public class EventActivity extends AppCompatActivity {
     private void showDate(int year, int month, int day) {
         startDate.setText(new StringBuilder().append(day).append("/")
                 .append(month).append("/").append(year));
+    }
+
+    public void showTimePickerDialog(View v) {
+        DialogFragment newFragment = new TimePickerFragment();
+        newFragment.show(getSupportFragmentManager(), "timePicker");
+    }
+
+    public static class TimePickerFragment extends DialogFragment
+            implements TimePickerDialog.OnTimeSetListener {
+
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            // Use the current time as the default values for the picker
+            final Calendar c = Calendar.getInstance();
+            int hour = c.get(Calendar.HOUR_OF_DAY);
+            int minute = c.get(Calendar.MINUTE);
+
+            // Create a new instance of TimePickerDialog and return it
+            return new TimePickerDialog(getActivity(), this, hour, minute,
+                    DateFormat.is24HourFormat(getActivity()));
+        }
+
+        public void onTimeSet(TimePicker view, int hourOfDay, int minuteOfDay) {
+            // Do something with the time chosen by the user
+            hour = hourOfDay;
+            minute = minuteOfDay;
+
+        }
+
+
+    }
+
+    private void checkPermissionsAndOpenFilePicker() {
+        String permission = Manifest.permission.READ_EXTERNAL_STORAGE;
+
+        if (ContextCompat.checkSelfPermission(this, permission) != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, permission)) {
+                showError();
+            } else {
+                ActivityCompat.requestPermissions(this, new String[]{permission}, PERMISSIONS_REQUEST_CODE);
+            }
+        } else {
+            openFilePicker();
+        }
+    }
+
+    private void showError() {
+        Toast.makeText(this, "Allow external storage reading", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           @NonNull String permissions[], @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case PERMISSIONS_REQUEST_CODE: {
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    openFilePicker();
+                } else {
+                    showError();
+                }
+            }
+        }
+    }
+
+    private void openFilePicker() {
+        new MaterialFilePicker()
+                .withActivity(this)
+                .withRequestCode(FILE_PICKER_REQUEST_CODE)
+                .withFilter(Pattern.compile(".*\\.jpg$"))
+                .withFilterDirectories(true)
+                .withHiddenFiles(true)
+                .withTitle("Sample title")
+                .start();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == FILE_PICKER_REQUEST_CODE && resultCode == RESULT_OK) {
+            String path = data.getStringExtra(FilePickerActivity.RESULT_FILE_PATH);
+
+            if (path != null) {
+                Log.d("Path: ", path);
+                Toast.makeText(this, "Picked file: " + path, Toast.LENGTH_LONG).show();
+            }
+        }
     }
 
 }
